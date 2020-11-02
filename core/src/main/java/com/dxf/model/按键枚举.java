55ac2e39fff1cb20a78830c23d75a997363d780f
jsgygujun <jsgygujun@gmail.com>
2020-11-02 16:21:34 +0800

    添加按键枚举。

---
+++ core/src/main/java/com/dxf/model/按键枚举.java
@@ -0,0 +1,33 @@
+package com.dxf.model;
+
+public enum 按键枚举 {
+    X(0, "x"),
+    C(0, "c"),
+    Z(0, "z"),
+    V(0, "v"),
+    Q(0, "q"),
+    W(0, "w"),
+    E(0, "e"),
+    R(0, "r"),
+    T(0, "t"),
+    Y(0, "y"),
+    A(0, "a"),
+    S(0, "s"),
+    D(0, "d"),
+    F(0, "f"),
+    G(0, "g"),
+    H(0, "h"),
+    方向上(0, "up"),
+    方向下(0, "down"),
+    方向左(0, "left"),
+    方向右(0, "right");
+
+    int vkCode;
+    String strCode;
+
+    按键枚举(int vkCode, String strCode) {
+        this.vkCode = vkCode;
+        this.strCode = strCode;
+    }
+
+}
